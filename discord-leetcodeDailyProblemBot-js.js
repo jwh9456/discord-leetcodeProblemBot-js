602540c9ef33da63b80a79c6e0bfdb59ecb849e7
UHYEOK <49363292+jwh9456@users.noreply.github.com>
2023-11-16 17:14:24 +0900

    Update discord-leetcodeDailyProblemBot-js.js

--- discord-leetcodeDailyProblemBot-js.js
+++ discord-leetcodeDailyProblemBot-js.js
@@ -1,31 +1,42 @@
 import Discord from "discord.js";
-import { LeetCode } from "leetcode-query";
+import {LeetCode} from "leetcode-query";
 import cron from "node-cron";
 
-const leetcode = new LeetCode();
-const client = new Discord.Client();
 
-// When the client is ready, we will start listening for messages
-client.once("ready", () => {
-  console.log("Ready!");
-  cron.schedule("0 9 * * *", async () => {
-    try {
+const intents = new Discord.IntentsBitField(32767);
+const client = new Discord.Client({ intents: intents });
 
-      let dailyProblem = await leetcode.daily();
-      let embed = await new Discord.MessageEmbed()
+const alert = async (event, context) => {
+  try {
+    await client.login("botkey");
+    client.on("ready", async () => {
+      try {
+        const leetcode = new LeetCode();
+        let dailyProblem = await leetcode.daily();
+        let embed = await new Discord.EmbedBuilder()
+          .setTitle(`${dailyProblem.question.title}`)
+          .setURL(`https://leetcode.com${dailyProblem.link}`)
+          .setDescription(`Difficulty: ${dailyProblem.question.difficulty}`)
+          .setColor(0x00AE86);
 
-        .setTitle(`${dailyProblem.question.title}`)
-        .setURL(`https://leetcode.com${dailyProblem.link}`)
-        .setDescription(`Difficulty: ${dailyProblem.question.difficulty}`);
+        let channel = await client.channels.cache.get("chKey");
+        if (channel) {
+          await channel.send({ embeds: [embed] });
+          await client.destroy();
+          process.exit();
+        } else {
+          console.error("Channel not found");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
 
-      await client.channels.cache.get("1067369939637370884").send(embed);
-      // await message.channel.send(embed);
-    } catch (error) {
-      await client.channels.cache.get("1067369939637370884").send(error);
-      console.log(error);
-    }
-  })
-});
+    return { statusCode: 200, body: "Success" };
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 500, body: "Error" };
+  }
+};
 
-// Start the client
-client.login("myBotKey");
+cron.schedule('* 9 * * *', alert());
